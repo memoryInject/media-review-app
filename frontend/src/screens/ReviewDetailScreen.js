d5bdf42d017403c3a52a1647e3131c74f2b70452
mahesh ms <msmahesh007@gmail.com>
2021-08-25 22:18:10 +0800

    MRA-6-review-detail-page-placeholder-for-components

--- frontend/src/screens/ReviewDetailScreen.js
+++ frontend/src/screens/ReviewDetailScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import ReactPlayer from 'react-player';
+
+// import ReactPlayerComp from '../components/ReactPlayerComp';
 
 const ReviewDetailScreen = () => {
+  const url =
+    'https://media.istockphoto.com/videos/beautiful-universally-cloudscape-background-time-lapse-video-id1156186888';
   return (
-    <div>
-      <h1>Review Detail Screen</h1>
-    </div>
+    <>
+      <Row className='top-row'>
+        <Col xs lg='8'>
+          <div className='player-wrapper'>
+            <ReactPlayer url={url} width='100%' height='100%' />
+          </div>
+          <div className='text-center my-3 feedback'>
+            <h1>Feedback</h1>
+          </div>
+        </Col>
+        <Col xs lg='2' md='auto'>
+          <h1>Comments</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='play-list-bottom'>
+          <h1>Playlist</h1>
+        </Col>
+      </Row>
+    </>
   );
 };
 
