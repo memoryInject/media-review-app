a5bd9542e0b3a61274c112c9575208658c2b8df0
mahesh ms <msmahesh007@gmail.com>
2021-08-26 18:36:11 +0800

    updated django settings

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$@*5cvfugzjl!((0r8o0+2be5)th^z!p#+18vboo_^23sufx%'
+SECRET_KEY = os.environ.get('DJANGO_SECRECT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
