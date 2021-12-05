26b6b9d9966d541c964bb46f42be5efb8c611afe
mahesh ms <msmahesh007@gmail.com>
2021-12-05 20:44:52 +0800

    updated release-tasks

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,6 @@
 NPM_CONFIG_PRODUCTION=false npm install
 mkdir -p backend/staticfiles
+if [ -d "backend/client/build" ]; then rm -Rf backend/client/build; fi
 npm run heroku-build
 npm run heroku-post-build
 npm run load-data-dev
