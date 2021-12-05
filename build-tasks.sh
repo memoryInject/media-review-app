e989a1a42faa4294b51ae6885f9cfe717b4b40ff
mahesh ms <msmahesh007@gmail.com>
2021-12-05 23:59:46 +0800

    heroku tasks updated

--- build-tasks.sh
+++ build-tasks.sh
@@ -4,4 +4,5 @@ mv -f frontend/build ./backend/client/
 cd backend/client/build
 cp asset-manifest.json manifest.json service-worker.js service-worker.js.map robots.txt favicon.ico *.png *.xml *.svg ./static
 cd ../../
+if [ ! -e "db.sqlite3" ]; then touch db.sqlite3; fi
 python manage.py collectstatic
