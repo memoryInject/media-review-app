9e05c6dd43c719540551a03f856d011b08840857
mahesh ms <msmahesh007@gmail.com>
2021-12-05 22:05:56 +0800

    heroku tasks updated

---
+++ build-tasks.sh
@@ -0,0 +1,5 @@
+npm install --prefix frontend && npm run build --prefix frontend
+if [ -d "backend/client/build" ]; then rm -Rf backend/client/build; fi
+mv -f frontend/build ./backend/client/
+cd backend/client/build
+cp asset-manifest.json manifest.json service-worker.js service-worker.js.map robots.txt favicon.ico *.png *.xml *.svg ./static
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,6 +1,3 @@
-NPM_CONFIG_PRODUCTION=false npm install
 mkdir -p backend/staticfiles
-if [ -d "backend/client/build" ]; then rm -Rf backend/client/build; fi
-npm run heroku-build
-npm run heroku-post-build
+npm run heroku-release
 npm run load-data-dev
