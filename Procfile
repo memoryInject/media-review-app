58fe311556da2680987d7bbcc8cc0516e0395994
mahesh ms <msmahesh007@gmail.com>
2021-12-05 16:26:49 +0800

    prepare for heroku deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: ./release-tasks.sh
+web: cd backend && gunicorn config.wsgi
---
+++ release-tasks.sh
@@ -0,0 +1,4 @@
+NPM_CONFIG_PRODUCTION=false npm install
+npm run heroku-build
+npm run heroku-post-build
+npm run load-data-dev
