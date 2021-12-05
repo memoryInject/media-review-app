8694b32eae8575becbbc37d9bc3fbeeec694df9d
mahesh ms <msmahesh007@gmail.com>
2021-12-05 23:02:29 +0800

    heroku tasks updated

--- build-tasks.sh
+++ build-tasks.sh
@@ -3,3 +3,5 @@ if [ -d "backend/client/build" ]; then rm -Rf backend/client/build; fi
 mv -f frontend/build ./backend/client/
 cd backend/client/build
 cp asset-manifest.json manifest.json service-worker.js service-worker.js.map robots.txt favicon.ico *.png *.xml *.svg ./static
+cd ../../
+python manage.py collectstatic
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,2 @@
 python backend/manage.py migrate
-python backend/manage.py collectstatic
-npm run heroku-release
 npm run load-data-dev
